8f067809885dd55810de83888303f89fb1c758c6
Noah <bigspaceships56@gmail.com>
2026-01-20 10:57:56 -0500

    serving static files

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"embed"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/static"
 )
 
+//go:embed static
+var server embed.FS
+
 func main() {
+	fs, err := static.EmbedFolder(server, "static")
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
+	r.Use(static.Serve("/", fs))
+
+	r.GET("/api/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
 	r.Run()
-
 }
